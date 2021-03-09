90fdcffdb9aa31b9552af8014ae2a4ec714b6b32
DenverLacey <36467790+DenverLacey@users.noreply.github.com>
2021-03-09 14:24:49 +1100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+using System;
+using SplashKitSDK;
+
+public class Program
+{
+    public static void Main()
+    {
+        new Window("Shape Drawer", 800, 600);
+        Shape shape = new Shape(Color.Red, 300, 200, 100, 100);
+
+        do
+        {
+            SplashKit.ProcessEvents();
+            SplashKit.ClearScreen();
+
+            shape.Draw();
+
+            if (SplashKit.MouseClicked(MouseButton.LeftButton))
+            {
+                shape.X = SplashKit.MouseX();
+                shape.Y = SplashKit.MouseY();
+            }
+
+            if (SplashKit.KeyTyped(KeyCode.SpaceKey) && shape.IsAt(SplashKit.MousePosition()))
+            {
+                Color newColor = SplashKit.RandomRGBColor(255);
+                shape.Color = newColor;
+            }
+
+            if (SplashKit.KeyTyped(KeyCode.EscapeKey))
+            {
+                SplashKit.CloseAllWindows();
+            }
+
+            SplashKit.RefreshScreen();
+
+        } while (!SplashKit.WindowCloseRequested("Shape Drawer"));
+    }
+}
---
+++ Shape.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using SplashKitSDK;
+
+public class Shape
+{
+	private Color color;
+	private float x;
+	private float y;
+	private int width;
+	private int height;
+
+	public Color Color { get => color; set => color = value; }
+	public float X { get => x; set => x = value; }
+	public float Y { get => y; set => y = value; }
+	public int Width { get => width; set => width = value; }
+	public int Height { get => height; set => height = value; }
+
+	public Shape(Color color, float x, float y, int width, int height)
+	{
+		this.color = color;
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+
+	public void Draw()
+	{
+		SplashKit.FillRectangle(color, x, y, width, height);
+    }
+
+	public bool IsAt(Point2D pt)
+    {
+		return pt.X >= x && pt.X <= x + width &&
+			pt.Y >= y && pt.Y <= y + height;
+    }
+}
